c386d0f6b49e4a082acfd3ca0509a55c616c7725
Zwe Nyan Win <zwenyanwin@Zwes-MacBook-Air-2.local>
2025-05-17 22:39:03 +0700

    self study

--- src/main/java/com/clinic/appointment/controller/DoctorController.java
+++ src/main/java/com/clinic/appointment/controller/DoctorController.java
@@ -1,16 +1,22 @@
 package com.clinic.appointment.controller;
 
 import com.clinic.appointment.model.Doctor;
+import com.clinic.appointment.service.DoctorService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/doctors")
+@RequiredArgsConstructor
 public class DoctorController {
+    private final DoctorService doctorService;
+    @GetMapping
+    public String getDoctors(Model model){
+        model.addAttribute("doctors" , doctorService.findAll());
+        return "doctors/listing";
+    }
 
     @GetMapping("/new")
     public String showCreateForm(Model model){
@@ -20,7 +26,40 @@ public class DoctorController {
 
     @PostMapping("/create")
     public String createDoctor(@ModelAttribute Doctor doctor){
-
-        return "doctors/create";
+        doctorService.create(doctor);
+        return "redirect:/doctors";
     }
+
+
+    //when hit edit button in listing.html
+    @GetMapping("/edit/{id}")
+    public String showEditForm(@PathVariable("id") Long id, Model model) {
+        Doctor doctor = this.doctorService.findById(id);
+        if (doctor == null) {
+            // handle not found case (optional)
+            return "redirect:/doctors";
+        }
+        model.addAttribute("doctor", doctor);
+        return "doctors/edit"; // This should be your edit form view
+    }
+
+    //when hit the update button
+    @PostMapping("/update/{id}")
+    public String updateDoctor(@ModelAttribute("doctor") Doctor doctor , @PathVariable("id") Long id) {
+        doctorService.update(id ,doctor);
+        return "redirect:/doctors";
+    }
+
+    //for deleting
+    @GetMapping("/delete/{id}")
+    public String deleteDoctor(@PathVariable("id") Long id) {
+        doctorService.deleteById(id);
+        return "redirect:/doctors";
+    }
+
+
+
+
+
+
 }
--- src/main/java/com/clinic/appointment/service/DoctorService.java
+++ src/main/java/com/clinic/appointment/service/DoctorService.java
@@ -2,29 +2,45 @@ package com.clinic.appointment.service;
 
 import com.clinic.appointment.model.Doctor;
 import com.clinic.appointment.repository.DoctorRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
+@AllArgsConstructor
 public class DoctorService {
 
     private final DoctorRepository doctorRepository;
 
-    public DoctorService(DoctorRepository doctorRepository) {
-        this.doctorRepository = doctorRepository;
+    public void create(Doctor doctor){
+        doctor = this.doctorRepository.save(doctor);
     }
 
-    public Doctor create(Doctor doctor){
-        doctor = this.doctorRepository.save(doctor);
-        return doctor;
+    public Doctor update(long id,Doctor doctor) {
+        Optional<Doctor> updateDoctorOp = this.doctorRepository.findById(id);
+        if (!updateDoctorOp.isEmpty()) {
+            Doctor updateDoctor = updateDoctorOp.get();
+            updateDoctor.setName(doctor.getName());
+            updateDoctor.setAddress(doctor.getAddress());
+            updateDoctor.setPhone(doctor.getPhone());
+            doctor = this.doctorRepository.save(updateDoctor);
+            return doctor;
+        }
+        return null;
     }
 
-    public Doctor update(Doctor doctor){
-        doctor = this.doctorRepository.save(doctor);
-        return doctor;
+    public List<Doctor> findAll(){
+        return this.doctorRepository.findAll();
     }
 
     public Doctor findById(Long id){
         return this.doctorRepository.findById(id).orElseThrow();
+    }
+
+    public void deleteById(Long id) {
+        doctorRepository.deleteById(id);
     }
 
 }
